ee504ba6b6176ea6f0d5e556f5a3296b2dcd243b
JoelBaanen <99074905@mydavinci.nl>
2023-03-21 10:20:05 +0100

    horeca app af!

--- Module1/horeca_app/horecaapp.js
+++ Module1/horeca_app/horecaapp.js
@@ -1,12 +1,51 @@
-let x = true
-let totaal = 0
-while (x = true){
-bestelling = prompt(`Wat wilt u bestellen? (Type 'stop' om te stoppen.)\n`).toLowerCase()
-bon = {"bier":4.00 , "fris":3.00,"wijn":5.50}
-if (bestelling == "stop"){
-    x = false
+const PRIJS_FRIS = 2.00;
+const PRIJS_BIER = 2.50;
+const PRIJS_WIJN = 3.00;
+var bonnetje = [];
+
+while (true) {
+let product = prompt("Welk product wil je toevoegen? (of typ 'stop' om te stoppen)");
+if (product == "stop") {
+break;
+} else {
+if (product !== "fris" && product !== "bier" && product !== "wijn") {
+alert("Dit ken ik niet.");
+} else {
+let aantal = prompt("Hoeveel " + product + " wil je hebben?");
+let gevonden = false;
+for (var i = 0; i < bonnetje.length; i++) {
+if (bonnetje[i].product == product) {
+bonnetje[i].aantal = parseInt(bonnetje[i].aantal) + parseInt(aantal);
+gevonden = true;
+break;
+    }
 }
+if (!gevonden) {
+bonnetje.push({ product: product, aantal: aantal });
 }
-print("----------------------------------")
-print(bon)
-print("----------------------------------")
+alert(aantal + " " + product + " is toegevoegd aan het bonnetje.");
+        }
+    }
+}
+
+if (bonnetje.length > 0) {
+var bonnetjeOutput = "Bonnetje:<br>";
+var totaalprijs = 0;
+for (var i = 0; i < bonnetje.length; i++) {
+var product = bonnetje[i].product;
+var aantal = bonnetje[i].aantal;
+var prijs = 0;
+if (product === "fris") {
+prijs = PRIJS_FRIS;
+} else if (product === "bier") {
+prijs = PRIJS_BIER;
+} else if (product === "wijn") {
+prijs = PRIJS_WIJN;
+    }
+var productTotaal = prijs * aantal;
+bonnetjeOutput += aantal + " " + product + " (" + prijs.toFixed(2) + " euro/stuk) = " + productTotaal.toFixed(2) + " euro<br>";
+totaalprijs += productTotaal;
+    }
+bonnetjeOutput += "Totaal: " + totaalprijs.toFixed(2) + " euro";
+document.getElementById("output").innerHTML = bonnetjeOutput;
+    }
